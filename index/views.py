e5e8edb47b84325cd3154755602f086444f16aee
Denis <denisyurievich94@gmail.com>
2017-04-01 15:51:36 +0300

    update

--- index/views.py
+++ index/views.py
@@ -164,6 +164,8 @@ def callback(request):
         u.set_password('password')
         u.save()
         u.counter_set.create()
+        user = authenticate(username=json_obj['login'], password='password')
+        login(request, user)
     except:
         #return render(request, 'registration/sign_up.html', context)
         return HttpResponse('This user already exists.')
