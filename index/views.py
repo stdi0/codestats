e7a68236b94df68e2004b031da30dcb9be7e3948
Denis <denisyurievich94@gmail.com>
2017-04-02 20:16:45 +0300

    update

--- index/views.py
+++ index/views.py
@@ -177,8 +177,8 @@ def callback(request):
             counter.save()
         qset.counter__github_login = json_obj['login']
         qset.save()
-        #return HttpResponseRedirect(reverse('index'))
-        return HttpResponse(user.counter__github_login)
+        return HttpResponseRedirect(reverse('index'))
+        #return HttpResponse(user.counter__github_login)
 
     user = User.objects.filter(counter__github_login=json_obj['login'])
     #user = ''
